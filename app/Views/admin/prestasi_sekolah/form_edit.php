7c1b8c8f429ed7d66f6476dbdd87d00ef512f400
satrioed <32364936+satrio13@users.noreply.github.com>
2024-10-08 23:14:08 +0700

    Update form_edit.php

--- app/Views/admin/prestasi_sekolah/form_edit.php
+++ app/Views/admin/prestasi_sekolah/form_edit.php
@@ -156,16 +156,14 @@ $tingkat = old('tingkat') ?? $data->tingkat;
         
         function readURL(input)
         {
-            // Mulai membaca inputan gambar
-            if (input.files && input.files[0]) {
-                var reader = new FileReader(); // Membuat variabel reader untuk API FileReader
-
-                reader.onload = function (e) {
-                    // Mulai pembacaan file
-                    $("#preview_gambar") // Tampilkan gambar yang dibaca ke area id #preview_gambar
-                        .attr("src", e.target.result);
+            if(input.files && input.files[0])
+            {
+                var reader = new FileReader();
+                reader.onload = function (e)
+                {
+                    $("#preview_gambar").attr("src", e.target.result);
                     //.width(300); // Menentukan lebar gambar preview (dalam pixel)
-                    //.height(200); // Jika ingin menentukan lebar gambar silahkan aktifkan perintah pada baris ini
+                    //.height(200); // // Menentukan tinggi gambar preview (dalam pixel)
                 };
 
                 reader.readAsDataURL(input.files[0]);
