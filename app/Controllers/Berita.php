1c29b7ca118fd6bb5596a82b6c3b0f1ace472761
satrioed <32364936+satrio13@users.noreply.github.com>
2024-10-08 23:20:06 +0700

    Update Berita.php

--- app/Controllers/Berita.php
+++ app/Controllers/Berita.php
@@ -40,11 +40,7 @@ class Berita extends BaseController
             $get = $this->m_berita->detail_berita($slug);
             $data['titleweb'] = $get->nama.' - '.title();
 			$data['title'] = $get->nama;
-
-            $upd = [
-                'dibaca' => $cek->dibaca + 1
-            ];
-            
+            $upd = ['dibaca' => $cek->dibaca + 1];
             $this->m_berita->update_dibaca($upd, $slug);
             $data['data'] = $get;
             $data['berita_populer'] = $this->m_home->berita_populer($slug);
